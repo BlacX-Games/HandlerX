41d25a75871e0cf5b18fc4123339fee02a48a704
Shayan <shayan.officeal@gmail.com>
2025-04-16 15:38:34 -0700

    feat: imghandler

---
+++ Assets/HandlerX/Scripts/IMGHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BlacXGames
+{
+    public class IMGHandler : MonoBehaviour
+    {
+        private Image _image;
+        
+        private void Awake() => _image = GetComponent<Image>();
+        
+        public void SetSprite(Sprite sprite) => _image.sprite = sprite;
+        public void SetColor(Color color) => _image.color = color;
+        public void Activate() => gameObject.SetActive(true);
+        public void Deactivate() => gameObject.SetActive(false);
+    }
+}
