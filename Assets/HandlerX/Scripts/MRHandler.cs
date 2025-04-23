055b7a545aa3bd6a4d899057dc3b2fc165a29e6d
Shayan <shayan.officeal@gmail.com>
2025-04-23 11:39:37 -0700

    feat: implement core logic in MRHandler

--- Assets/HandlerX/Scripts/MRHandler.cs
+++ Assets/HandlerX/Scripts/MRHandler.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MRHandler : MonoBehaviour
+namespace BlacXGames
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MRHandler : MonoBehaviour
     {
-        
-    }
+        private MeshRenderer _meshRenderer;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void Awake() => _meshRenderer = GetComponent<MeshRenderer>();
         
-    }
+        public void Activate() => gameObject.SetActive(true);
+        public void Deactivate() => gameObject.SetActive(false);
+    }   
 }
