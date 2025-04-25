6644ee2d1f9428bb6da8e1e9217470e9f21175ce
Shayan <shayan.officeal@gmail.com>
2025-04-25 11:15:48 -0700

    zfeat: add new handler script for Rigidbody2D component

---
+++ Assets/Plugins/BlacX Games/HandlerX/Scripts/RB2DHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlacXGames
+{
+    public class RB2DHandler : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }   
+}
