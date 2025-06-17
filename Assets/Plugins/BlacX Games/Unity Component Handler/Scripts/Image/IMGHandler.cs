145521fda01d2b939789071c71b97c9c0bd73f70
Shayan <shayan.officeal@gmail.com>
2025-06-17 09:33:11 -0700

    feat: fill amount, helpers for min and max of fill amount

--- Assets/Plugins/BlacX Games/Unity Component Handler/Scripts/Image/IMGHandler.cs
+++ Assets/Plugins/BlacX Games/Unity Component Handler/Scripts/Image/IMGHandler.cs
@@ -14,6 +14,9 @@ namespace BlacXGames.Plugins.Utilities.Uch
         
         public void SetSprite(Sprite sprite) => _image.sprite = sprite;
         public void SetColor(Color color) => _image.color = color;
+        public void SetFillAmount(float amount) => _image.fillAmount = amount;
+        public void SetFillAmountToMin(float amount) => _image.fillAmount = 0;
+        public void SetFillAmountToMax(float amount) => _image.fillAmount = 1;
         
         public static void SetSprite(IMGHandler imgHandler, Sprite sprite) => imgHandler.SetSprite(sprite);
         public static void SetColor(IMGHandler imgHandler, Color color) => imgHandler.SetColor(color);
