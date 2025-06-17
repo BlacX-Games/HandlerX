8f0562c3d8265cd5464fa8f109e750442c63653d
Shayan <shayan.officeal@gmail.com>
2025-06-17 09:56:10 -0700

    feat: static methods

--- Assets/Plugins/BlacX Games/Unity Component Handler/Scripts/Image/IMGHandler.cs
+++ Assets/Plugins/BlacX Games/Unity Component Handler/Scripts/Image/IMGHandler.cs
@@ -23,10 +23,21 @@ namespace BlacXGames.Plugins.Utilities.Uch
         public void SetTypeToTiled() => _image.type = Image.Type.Tiled;
         public void SetTypeToFilled() => _image.type = Image.Type.Filled;
         public void SetFillAmount(float amount) => _image.fillAmount = amount;
-        public void SetFillAmountToMin(float amount) => _image.fillAmount = 0;
-        public void SetFillAmountToMax(float amount) => _image.fillAmount = 1;
+        public void SetFillAmountToMin() => _image.fillAmount = 0;
+        public void SetFillAmountToMax() => _image.fillAmount = 1;
         
         public static void SetSprite(IMGHandler imgHandler, Sprite sprite) => imgHandler.SetSprite(sprite);
         public static void SetColor(IMGHandler imgHandler, Color color) => imgHandler.SetColor(color);
+        public static void SetMaterial(IMGHandler imgHandler, Material material) => imgHandler.SetMaterial(material);
+        public static void SetRaycastTarget(IMGHandler imgHandler, bool state) => imgHandler.SetRaycastTarget(state);
+        public static void SetMaskable(IMGHandler imgHandler, bool state) => imgHandler.SetMaskable(state);
+        public static void SetType(IMGHandler imgHandler, Image.Type type) => imgHandler.SetType(type);
+        public static void SetTypeToSimple(IMGHandler imgHandler) => imgHandler.SetTypeToSimple();
+        public static void SetTypeToSliced(IMGHandler imgHandler) => imgHandler.SetTypeToSliced();
+        public static void SetTypeToTiled(IMGHandler imgHandler) => imgHandler.SetTypeToTiled();
+        public static void SetTypeToFilled(IMGHandler imgHandler) => imgHandler.SetTypeToFilled();
+        public static void SetFillAmount(IMGHandler imgHandler, float amount) => imgHandler.SetFillAmount(amount);
+        public static void SetFillAmountToMin(IMGHandler imgHandler) => imgHandler.SetFillAmountToMin();
+        public static void SetFillAmountToMax(IMGHandler imgHandler) => imgHandler.SetFillAmountToMax();
     }
 }
